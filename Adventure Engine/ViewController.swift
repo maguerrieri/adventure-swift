5a4488d604ab72ac76679b9fa152d9d964c0b353
ClockworkPirate <mguerrieri@outlook.com>
2015-07-18 18:38:34 -0700

    Initial Commit

---
+++ Adventure Engine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Adventure Engine
+//
+//  Created by Mama Luigi on 7/18/15.
+//  Copyright © 2015 Igneus. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
